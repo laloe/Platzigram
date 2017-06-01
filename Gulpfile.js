6975b4f95e13848a2a920d54769d0ad779b8f77b
Eduardo González <laloe.90@gmail.com>
2017-06-01 17:38:44 -0500

    trabajando en el archivo gulp.js

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,6 +4,7 @@ var rename = require('gulp-rename');
 var babel = require('babelify');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var watchify = require('watchify');
 
 gulp.task('styles', function () {
     gulp
@@ -11,21 +12,43 @@ gulp.task('styles', function () {
         .pipe(sass())
         .pipe(rename('app.css'))
         .pipe(gulp.dest('public'));
-})
+});
 
 gulp.task('assets', function () {
     gulp
         .src('assets/*')
         .pipe(gulp.dest('public'));
-})
+});
 
-gulp.task('scripts', function () {
-    browserify('./src/index.js') 
-        .transform(babel)
-        .bundle()
-        .pipe(source('index.js'))
-        .pipe(rename('app.js'))
-        .pipe(gulp.dest('public'));
-})
+function compile(watch) {
+    var bundle = watchify(browserify('./src/index.js'));
 
-gulp.task('default', ['styles', 'assets', 'scripts']);
+    function rebundle() {
+        bundle
+            .transform(babel)
+            .bundle()
+            .pipe(source('index.js'))
+            .pipe(rename('app.js'))
+            .pipe(gulp.dest('public'));
+
+    }
+
+    if (watch) {
+        bundle.on('update', function () {
+            console.log('--> Bundling..');
+        })
+    }
+
+    rebundle()
+}
+
+gulp.task('build', function () {
+    return compile();
+});
+
+gulp.task('watch', function (params) {
+    return compile(true); 
+});
+
+
+gulp.task('default', ['styles', 'assets', 'build']);
